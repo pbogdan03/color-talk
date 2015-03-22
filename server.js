ae8df0d45dadbcd5896882f8bd5ce929fa5bbd46
Bogdan <p_bogdan03@yahoo.com>
2015-03-22 12:55:23 +0200

    Favicon test

--- server.js
+++ server.js
@@ -20,15 +20,14 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
-
 mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
